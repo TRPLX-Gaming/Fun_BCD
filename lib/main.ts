b76ed2e11e72232b3cbed7604dcb7add32dcb51d
TRPLX_Gaming <icgabriel2007@gmail.com>
2026-02-20 17:42:42 +0100

    basic ui

--- lib/main.ts
+++ lib/main.ts
@@ -1,9 +1,15 @@
+// imports
 import express,{Request, Response} from 'express'
+import path from 'path'
 
+// constants
 const app = express()
+const PATH = path.join(__dirname,'../public')
+const TARGET = path.join(PATH,'/index.html')
 
+// APIs
 app.get('/',(req:Request, res:Response) => {
-  res.send('200')
+  res.sendFile(TARGET)
 })
 
 app.listen(3000,()=>console.log('active on port 3000'))
--- src/main.js
+++ src/main.js
@@ -4,9 +4,12 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const path_1 = __importDefault(require("path"));
 const app = (0, express_1.default)();
+const PATH = path_1.default.join(__dirname, '../public');
+const TARGET = path_1.default.join(PATH, '/index.html');
 app.get('/', (req, res) => {
-    res.send('200');
+    res.sendFile(TARGET);
 });
 app.listen(3000, () => console.log('active on port 3000'));
 console.log('ok');
